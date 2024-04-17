387a90138b6721d2f0066ea8f3e95e1982ddb927
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-04-17 23:40:19 +0530

    updates

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { GoogleLogin } from '@react-oauth/google';
+
+function App() {
+  return (
+    <GoogleLogin
+    onSuccess={credentialResponse => {
+      console.log(credentialResponse);
+    }}
+    onError={() => {
+      console.log('Login Failed');
+    }}
+  />
+  );
+}
+
+export default App;
